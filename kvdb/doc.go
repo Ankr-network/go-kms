da3647db2488386148bfa41ddc2e714aa7a5706a
mobius <sv0220@163.com>
2020-01-07 11:06:59 +0800

    update documents

--- kvdb/doc.go
+++ kvdb/doc.go
@@ -1,5 +1,5 @@
 /*
-	Package kvdb is go client library for dealing with Ankr KMS service
-it includes a high-level API for easy store static secret information
+	Package kvdb is go client library for dealing with Ankr KMS service. it includes a high-level API for easy store
+static secret information
 */
 package kvdb
--- pki/doc.go
+++ pki/doc.go
@@ -1,9 +1,7 @@
-//
-// Packge pki is a pure go client for dealing with Ankr KMS.
-// it includes a high-level API for easy generate private key and public key
-//
-// Warning
-// a new client will be invalid, when the token is invalid, it makes us to create
-// a new client, please care it.
+/*
+	Packge pki is a pure go client for dealing with Ankr KMS.
+it includes a high-level API for easy generate private key and public key Warning a new client will be invalid,
+when the token is invalid, it makes us to create a new client, please care it.
+*/
 
 package pki
