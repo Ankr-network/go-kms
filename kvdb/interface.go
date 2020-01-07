d6e07172aeeee7d57eb0a136bcabc6af49f65f64
mobius <sv0220@163.com>
2020-01-07 13:43:54 +0800

    update documents

--- kvdb/interface.go
+++ kvdb/interface.go
@@ -1,9 +1,10 @@
 package kvdb
 
 /*
-   kvdb api constraint: we store anything, the data must format map, thank you know.
-we thought to replace map structure with []byte structure, but for extensions, we think the map is well,
-the map is a stable structure. think about some scenario of only take part of the secrets, we can easy handle with it.
+kvdb api constraint: we store anything, the data must format map, thank you know.
+we thought to replace map structure with []byte structure, but for extensions, we
+think the map is well,the map is a stable structure. think about some scenario of
+only take part of the secrets, we can easy handle with it.
 */
 type KVer interface {
 	// take secrets by specify path
